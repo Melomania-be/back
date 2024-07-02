9453ef60ed8229e113de02229b4df48d9c514888
Califano9577 <amaury.giot@student.junia.com>
2024-07-02 13:01:40 +0200

    ajout instrument au recommended

--- app/validators/recommend_someone.ts
+++ app/validators/recommend_someone.ts
@@ -4,8 +4,17 @@ export const createRecommendedValidator = vine.compile(
   vine.object({
     first_name: vine.string(),
     last_name: vine.string(),
-    email: vine.string(),
-    phone: vine.string(),
-    messenger: vine.string(),
+    email: vine.string().optional(),
+    phone: vine.string().optional(),
+    messenger: vine.string().optional(),
+    instruments: vine
+      .array(
+        vine.object({
+          id: vine.number(),
+          name: vine.string(),
+          pivot_proficiency_level: vine.string(),
+        })
+      )
+      .optional(),
   })
 )
