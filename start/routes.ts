8149a45109c068cffadc3ad0ef3adff662202ac2
BlueFear <bluefear1976@gmail.com>
2025-07-18 06:04:54 +0200

    little bug fix

--- start/routes.ts
+++ start/routes.ts
@@ -8,8 +8,6 @@
 */
 import router from '@adonisjs/core/services/router'
 import { middleware } from './kernel.js'
-import Accounting from '#models/accounting'
-import AccountingsController from '#controllers/accounting_controller'
 
 const AuditionsController = () => import('#controllers/auditions_controller')
 const UsersController = () => import('#controllers/users_controller')
@@ -33,6 +31,7 @@ const SectionsController = () => import('#controllers/sections_controller')
 const TemplateController = () => import('#controllers/template_controller')
 const DefaultTemplatesController = () => import('#controllers/default_templates_controller')
 const ExpenseCategoriesController = () => import('#controllers/expenses_categories_controller')
+const AccountingsController = () => import('#controllers/accounting_controller')
 const FilesystemController = () => import('#controllers/filesystem_controller')
 
 router.group(() => {
@@ -207,9 +206,9 @@ router.group(() => {
           router.delete('/:id', [ProjectsController, 'delete'])
           router.get('/:id/management', [ProjectsController, 'getDashboard'])
           router.get('/:id/management/attendance', [ProjectsController, 'getAttendance'])
-          router.get('/projects/:id/management/accounting', [AccountingsController, 'getAll'])
-          router.post('/projects/:id/management/accounting', [AccountingsController, 'createOrUpdate'])
-          router.delete('/projects/:id/management/accounting/:accountingId', [AccountingsController, 'delete'])
+          router.get('/:id/management/accounting', [AccountingsController, 'getAll'])
+          router.post('/:id/management/accounting', [AccountingsController, 'createOrUpdate'])
+          router.delete('/:id/management/accounting/:accountingId', [AccountingsController, 'delete'])
 
           // =============================================================================
           // GESTION DES PARTICIPANTS
