7567b086284d4525562917c1bd90ce71f550eec0
N-nketsiah <nketsiahnaomi8@gmail.com>
2026-06-25 14:07:41 +0200

    feat: add organization scoping to composers controller to prevent IDOR - Naomi

--- app/controllers/composers_controller.ts
+++ app/controllers/composers_controller.ts
@@ -1,5 +1,3 @@
-// import type { HttpContext } from '@adonisjs/core/http'
-
 import Composer from '#models/composer'
 import { HttpContext } from '@adonisjs/core/http'
 import { createComposerValidator } from '#validators/composer'
@@ -7,7 +5,11 @@ import { simpleFilter } from 'adonisjs-filters'
 
 export default class ComposersController {
   async getAll(ctx: HttpContext) {
-    let baseQuery = Composer.query()
+    const organizationId = ctx.auth.user?.organizationId
+
+    let baseQuery = Composer.query().if(organizationId, (query) =>
+      query.where('organization_id', organizationId!)
+    )
 
     let res = await simpleFilter(
       ctx,
@@ -26,31 +28,43 @@ export default class ComposersController {
 
   async createOrUpdate(ctx: HttpContext) {
     const data = await ctx.request.validateUsing(createComposerValidator)
+    const organizationId = ctx.auth.user?.organizationId
 
     if (data.id === undefined) {
-      return await Composer.create(data)
+      return await Composer.create({ ...data, organizationId })
     }
 
-    const composer = await Composer.firstOrCreate({ id: data.id }, data)
-
-    if (composer.$isLocal) {
-      return composer
-    }
+    const composer = await Composer.query()
+      .where('id', data.id)
+      .if(organizationId, (query) => query.where('organization_id', organizationId!))
+      .firstOrFail()
 
     composer.merge(data)
     await composer.save()
     return composer
   }
 
-  async delete({ params, response }: HttpContext) {
+  async delete({ params, response, auth }: HttpContext) {
+    const organizationId = auth.user?.organizationId
     const composerId = params.id
-    const composer = await Composer.findOrFail(composerId)
+
+    const composer = await Composer.query()
+      .where('id', composerId)
+      .if(organizationId, (query) => query.where('organization_id', organizationId!))
+      .firstOrFail()
+
     await composer.delete()
     return response.status(204)
   }
 
-  async getPieces({ params, response }: HttpContext) {
-    const composer = await Composer.query().where('id', params.id).preload('pieces').first()
+  async getPieces({ params, response, auth }: HttpContext) {
+    const organizationId = auth.user?.organizationId
+
+    const composer = await Composer.query()
+      .where('id', params.id)
+      .if(organizationId, (query) => query.where('organization_id', organizationId!))
+      .preload('pieces')
+      .first()
 
     if (!composer) {
       return response.status(404).send('Composer not found')
